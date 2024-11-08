17765f82acffd6bd4b983e4d55e71c0689e2f70f
ClaytonFish <113127327+ClaytonFish@users.noreply.github.com>
2024-11-08 12:58:42 -0700

    Update command-runner.ts

--- src/command-runner.ts
+++ src/command-runner.ts
@@ -1,119 +1,119 @@
-import * as vscode from "vscode";
 import { exec } from "child_process";
+import * as vscode from "vscode";
 import OutputChannel from "./output-channel";
-import { isCmdShell, getClickableLinksInMsg } from "./utils";
 import {
-  ICommandValue,
-  IExecOptions,
-  Nullable,
-  PreparedCommand,
-  StatusType,
+    ICommandValue,
+    IExecOptions,
+    Nullable,
+    PreparedCommand,
+    StatusType,
 } from "./types";
+import { getClickableLinksInMsg, isCmdShell } from "./utils";
 
 type ProcessHandleFn = (command: PreparedCommand) => void;
 
 interface IProcessHandlers {
-  onStarted: ProcessHandleFn;
-  onFinish: ProcessHandleFn;
+    onStarted: ProcessHandleFn;
+    onFinish: ProcessHandleFn;
 }
 
 class CommandRunner {
-  public isRunProcess: boolean = false;
+    public isRunProcess: boolean = false;
 
-  public constructor(private outputChannel: OutputChannel) {}
+    public constructor(private outputChannel: OutputChannel) { }
 
-  private resolveProcessSuccess(msg: string): StatusType {
-    this.outputChannel.showMessage(getClickableLinksInMsg(msg));
-    return StatusType.Success;
-  }
+    private resolveProcessSuccess(msg: string): StatusType {
+        this.outputChannel.showMessage(getClickableLinksInMsg(msg));
+        return StatusType.Success;
+    }
 
-  private resolveProcessError(msg: string): StatusType {
-    this.outputChannel.showError(getClickableLinksInMsg(msg));
-    return StatusType.Error;
-  }
+    private resolveProcessError(msg: string): StatusType {
+        this.outputChannel.showError(getClickableLinksInMsg(msg));
+        return StatusType.Error;
+    }
 
-  private runShellProcess(
-    cmdVal: string,
-    execOptions: Nullable<IExecOptions>
-  ): Promise<StatusType> {
-    return new Promise((resolve) => {
-      exec(cmdVal, execOptions, (_, stdout, stderr) => {
-        const statusType: StatusType =
-          stderr !== ""
-            ? this.resolveProcessError(String(stderr))
-            : this.resolveProcessSuccess(String(stdout));
+    private runShellProcess(
+        cmdVal: string,
+        execOptions: Nullable<IExecOptions>
+    ): Promise<StatusType> {
+        return new Promise((resolve) => {
+            exec(cmdVal, execOptions, (_, stdout, stderr) => {
+                const statusType: StatusType =
+                    stderr !== ""
+                        ? this.resolveProcessError(String(stderr))
+                        : this.resolveProcessSuccess(String(stdout));
 
-        resolve(statusType);
-      });
-    });
-  }
+                resolve(statusType);
+            });
+        });
+    }
 
-  private runVscodeTask(cmd: string | string[]): Promise<StatusType> {
-    const [primaryCmd, ...restOptions] = [cmd].flat();
-    return new Promise((resolve) => {
-      vscode.commands
-        .executeCommand<Nullable<string>>(primaryCmd, ...restOptions)
-        .then(
-          (fulfillMsg) => {
-            resolve(this.resolveProcessSuccess(String(fulfillMsg ?? "")));
-          },
-          (rejectMsg) => {
-            resolve(this.resolveProcessError(String(rejectMsg)));
-          }
+    private runVscodeTask(cmd: string | string[]): Promise<StatusType> {
+        const [primaryCmd, ...restOptions] = [cmd].flat();
+        return new Promise((resolve) => {
+            vscode.commands
+                .executeCommand<Nullable<string>>(primaryCmd, ...restOptions)
+                .then(
+                    (fulfillMsg) => {
+                        resolve(this.resolveProcessSuccess(String(fulfillMsg ?? "")));
+                    },
+                    (rejectMsg) => {
+                        resolve(this.resolveProcessError(String(rejectMsg)));
+                    }
+                );
+        });
+    }
+
+    private runProcess(
+        command: ICommandValue,
+        execOptions: Nullable<IExecOptions>
+    ): Promise<StatusType> {
+        const { type, value } = command;
+        if (isCmdShell(type, value)) {
+            return this.runShellProcess(value, execOptions);
+        }
+        return this.runVscodeTask(value);
+    }
+
+    private runCommand(
+        command: PreparedCommand,
+        handlers: IProcessHandlers
+    ): Promise<StatusType> {
+        this.isRunProcess = true;
+        handlers.onStarted(command);
+        const proccessPromise: Promise<StatusType> = this.runProcess(
+            command.cmd,
+            command.execOptions
         );
-    });
-  }
-
-  private runProcess(
-    command: ICommandValue,
-    execOptions: Nullable<IExecOptions>
-  ): Promise<StatusType> {
-    const { type, value } = command;
-    if (isCmdShell(type, value)) {
-      return this.runShellProcess(value, execOptions);
-    }
-    return this.runVscodeTask(value);
-  }
-
-  private runCommand(
-    command: PreparedCommand,
-    handlers: IProcessHandlers
-  ): Promise<StatusType> {
-    this.isRunProcess = true;
-    handlers.onStarted(command);
-    const proccessPromise: Promise<StatusType> = this.runProcess(
-      command.cmd,
-      command.execOptions
-    );
-    proccessPromise.finally(() => {
-      this.isRunProcess = false;
-      handlers.onFinish(command);
-    });
-    return proccessPromise;
-  }
-
-  public async runCommandsAsync(
-    commands: PreparedCommand[],
-    handlers: IProcessHandlers
-  ): Promise<Array<StatusType | Promise<StatusType>>> {
-    const proccessResult: Array<StatusType | Promise<StatusType>> = [];
-    for (const command of commands) {
-      const proccessPromise: Promise<StatusType> = this.runCommand(
-        command,
-        handlers
-      );
-      if (command.revert) {
-        vscode.commands.executeCommand("workbench.action.files.revert");
-      }
-      if (command.isAsync) {
-        proccessResult.push(proccessPromise);
-      } else {
-        proccessResult.push(await proccessPromise);
-      }
+        proccessPromise.finally(() => {
+            this.isRunProcess = false;
+            handlers.onFinish(command);
+        });
+        return proccessPromise;
     }
 
-    return proccessResult;
-  }
+    public async runCommandsAsync(
+        commands: PreparedCommand[],
+        handlers: IProcessHandlers
+    ): Promise<Array<StatusType | Promise<StatusType>>> {
+        const proccessResult: Array<StatusType | Promise<StatusType>> = [];
+        for (const command of commands) {
+            const proccessPromise: Promise<StatusType> = this.runCommand(
+                command,
+                handlers
+            );
+            if (command.isAsync) {
+                proccessResult.push(proccessPromise);
+            } else {
+                proccessResult.push(await proccessPromise);
+            }
+            if (command.revert) {
+                vscode.commands.executeCommand("workbench.action.files.revert");
+            }
+        }
+
+        return proccessResult;
+    }
 }
 
 export default CommandRunner;
